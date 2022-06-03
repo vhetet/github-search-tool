c4240c59708c42b0216da507bb5a2c4935001f46
vhetet <vh1030478@gmail.com>
2022-06-02 23:24:00 -0500

    add search on page load

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [searchResult, setSearchResult] = useState([]);
+  const [searchKey, setSeasrchKey] = useState("facebook");
+
+  useEffect(() => {
+    fetch(
+      "https://api.github.com/search/repositories?q=facebook&&s=stars&type=Repositories"
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        setSearchResult(res.items);
+      });
+  }, []);
   return (
     <div className="App">
       <h1>Welcome to Github Search</h1>
-      <input type="text" placeholder='tpye the search keyword here'/>
+      <input type="text" placeholder="tpye the search keyword here" />
+      <div className="search-results">
+        {searchResult && searchResult.map((res) => <p>{res.id}</p>)}
+      </div>
     </div>
   );
 }
